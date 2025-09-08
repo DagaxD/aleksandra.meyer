13522c0955e0bb21ccaca5f71f92da96d6d66ad7
Dagmara Mierzejewska <dagmaratega@gmail.com>
2025-09-08 11:20:01 +0200

    change

--- src/components/TryMe/index.tsx
+++ src/components/TryMe/index.tsx
@@ -10,13 +10,13 @@ export const TryMe = () => {
             <div className='flex flex-row gap-[24px] justify-center flex-wrap bg-[#f6f2f9] p-[24px] shadow-md rounded-[12px] max-w-[900px] mx-auto items-center'>
                 <div className='max-w-[500px] text-left'>
                     <Typography variant='h6' className=' text-[#5a2a82]'>
-                        Zrób pierwszy krok – umów się na lekcję próbną i przekonaj się, jak skuteczna może być nauka języków w komfortowych warunkach.
+                        Zrób pierwszy krok – umów się na zajęcia próbne i przekonaj się, jak skuteczna może być nauka języków w komfortowych warunkach.
                     </Typography>
                 </div>
                 <div className="border text-[#5a2a82] h-[100px] mob-hide" />
                 <div className='justify-center items-center flex'>
                     <Button variant='contained' color='secondary' href='/kontakt' size='large'>
-                        Umów się na lekcję próbną
+                        Umów się na zajęcia próbne
                     </Button>
                 </div>
             </div>
